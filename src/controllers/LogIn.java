3916a7965889dd075520003bc6295b1ac5947bef
Sle3pingForest <huenam.ly@gmail.com>
2019-04-18 15:29:28 +0200

    login admin with DAO

--- src/controllers/LogIn.java
+++ src/controllers/LogIn.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.Utilisateur;
+import dao.UsersDAO;
 import forms.LogInForm;
 
 /**
@@ -24,6 +27,7 @@ public class LogIn extends HttpServlet {
     public static final String VUE_USERS             = "/WEB-INF/User.jsp";
     public static final String VUE_ADMIN            = "/admin/Admin.jsp";
     public static final String VUE_LOGIN            = "/LogIn.jsp";
+    public static Map<String,String> listAdmin  = null;
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /* Affichage de la page de connexion */
@@ -39,6 +43,7 @@ public class LogIn extends HttpServlet {
 
         /* Récupération de la session depuis la requête */
         HttpSession session = request.getSession();
+        
 
         /**
          * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
@@ -59,7 +64,7 @@ public class LogIn extends HttpServlet {
         System.out.println(utilisateur.getEmail());
        
         if(session.getAttribute(ATT_SESSION_USER) != null){
-        	 if(utilisateur.getEmail().equals("admin@admin.com")){
+        	 if(isAdmin(utilisateur.getEmail(), utilisateur.getMotDePasse())){
                  this.getServletContext().getRequestDispatcher(VUE_ADMIN).forward( request, response );
              }
              if(utilisateur.getEmail().equals("user@user.com")){
@@ -70,6 +75,27 @@ public class LogIn extends HttpServlet {
             this.getServletContext().getRequestDispatcher( VUE_LOGIN).forward( request, response );
         }
 
+    }
+    
+   
+    
+    private boolean isAdmin(String mail, String pass){
+    	boolean is_admin = false;
+    	boolean correct_pass = false;
+    	try {
+			listAdmin = UsersDAO.selectAdmin();
+			System.out.println(listAdmin.get(mail));
+			if(listAdmin.containsKey(mail) && listAdmin.get(mail).equals(pass)){
+	    		is_admin = true;
+	    		correct_pass = true;
+	    	}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return is_admin & correct_pass;
+    	
     }
 
 }
--- src/dao/UsersDAO.java
+++ src/dao/UsersDAO.java
@@ -6,21 +6,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UsersDAO  {
 	
 	protected static PreparedStatement preparedStatement = null;
 	protected static ResultSet resultSet = null;
 	protected static ArrayList<String> listUsers = null;
+	protected static ArrayList<String> listMail = null;
+	protected static Map<String,String> listAdmin = null;
 	protected static Statement statement = null;
 	
-	public static ArrayList<String>  selectAll() throws Exception{
+	public static ArrayList<String>  selectUsersName() throws Exception{
 
 		ConnexionBDD connect = ConnexionBDD.getInstance();
 
 		try{
 		    statement = connect.getCnx().createStatement();
-			preparedStatement = connect.getCnx().prepareStatement("SELECT * from ADMINS");
+			preparedStatement = connect.getCnx().prepareStatement("SELECT username from USERS");
 			resultSet = preparedStatement.executeQuery();
 			listUsers = new ArrayList<>();
 		    while (resultSet.next()) {
@@ -37,5 +41,54 @@ public class UsersDAO  {
 		
 		return listUsers;
 
+	}
+	
+	public static ArrayList<String>  selectUsersMail() throws Exception{
+
+		ConnexionBDD connect = ConnexionBDD.getInstance();
+
+		try{
+		    statement = connect.getCnx().createStatement();
+			preparedStatement = connect.getCnx().prepareStatement("SELECT email from USERS");
+			resultSet = preparedStatement.executeQuery();
+			listMail = new ArrayList<>();
+		    while (resultSet.next()) {
+		       String email = resultSet.getString("email");
+		       listMail.add(email);
+		    }
+		}
+		catch (Exception e) {
+		      throw e;
+		} 
+		finally {
+		      connect.closeCnx();
+		}
+		
+		return listMail;
+	}
+	
+	public static Map<String, String> selectAdmin()throws Exception{
+
+		ConnexionBDD connect = ConnexionBDD.getInstance();
+
+		try{
+		    statement = connect.getCnx().createStatement();
+			preparedStatement = connect.getCnx().prepareStatement("SELECT * from ADMINS");
+			resultSet = preparedStatement.executeQuery();
+			listAdmin = new HashMap<>();
+		    while (resultSet.next()) {
+		    	String name = resultSet.getString("username");
+		       String email = resultSet.getString("email");
+		       String pass = resultSet.getString("password");
+		       listAdmin.put(email , pass);
+		    }
+		}
+		catch (Exception e) {
+		      throw e;
+		} 
+		finally {
+		      connect.closeCnx();
+		}
+		return listAdmin;
 	}
 }
