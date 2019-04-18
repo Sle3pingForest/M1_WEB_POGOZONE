f776c2b1a59ad3cf380c4f346518327cd16bd139
Sle3pingForest <huenam.ly@gmail.com>
2019-04-18 16:22:41 +0200

    changement de base donne

--- src/dao/UsersDAO.java
+++ src/dao/UsersDAO.java
@@ -1,4 +1,5 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,85 +10,79 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UsersDAO  {
-	
+public class UsersDAO {
+
 	protected static PreparedStatement preparedStatement = null;
 	protected static ResultSet resultSet = null;
 	protected static ArrayList<String> listUsers = null;
 	protected static ArrayList<String> listMail = null;
-	protected static Map<String,String> listAdmin = null;
+	protected static Map<String, String> listAdmin = null;
 	protected static Statement statement = null;
-	
-	public static ArrayList<String>  selectUsersName() throws Exception{
+
+	public static ArrayList<String> selectUsersName() throws Exception {
 
 		ConnexionBDD connect = ConnexionBDD.getInstance();
 
-		try{
-		    statement = connect.getCnx().createStatement();
-			preparedStatement = connect.getCnx().prepareStatement("SELECT username from USERS");
+		try {
+			statement = connect.getCnx().createStatement();
+			preparedStatement = connect.getCnx().prepareStatement("SELECT NOM from USER WHERE EST_ADMIN IS NULL");
 			resultSet = preparedStatement.executeQuery();
 			listUsers = new ArrayList<>();
-		    while (resultSet.next()) {
-		       String user = resultSet.getString("username");
-		       listUsers.add(user);
-		    }
+			while (resultSet.next()) {
+				String user = resultSet.getString("NOM");
+				listUsers.add(user);
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			connect.closeCnx();
 		}
-		catch (Exception e) {
-		      throw e;
-		} 
-		finally {
-		      connect.closeCnx();
-		}
-		
+
 		return listUsers;
 
 	}
-	
-	public static ArrayList<String>  selectUsersMail() throws Exception{
+
+	public static ArrayList<String> selectUsersMail() throws Exception {
 
 		ConnexionBDD connect = ConnexionBDD.getInstance();
 
-		try{
-		    statement = connect.getCnx().createStatement();
-			preparedStatement = connect.getCnx().prepareStatement("SELECT email from USERS");
+		try {
+			statement = connect.getCnx().createStatement();
+			preparedStatement = connect.getCnx().prepareStatement("SELECT E_MAIL from USER WHERE EST_ADMIN IS NULL");
 			resultSet = preparedStatement.executeQuery();
 			listMail = new ArrayList<>();
-		    while (resultSet.next()) {
-		       String email = resultSet.getString("email");
-		       listMail.add(email);
-		    }
+			while (resultSet.next()) {
+				String email = resultSet.getString("E_MAIL");
+				listMail.add(email);
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			connect.closeCnx();
 		}
-		catch (Exception e) {
-		      throw e;
-		} 
-		finally {
-		      connect.closeCnx();
-		}
-		
+
 		return listMail;
 	}
-	
-	public static Map<String, String> selectAdmin()throws Exception{
+
+	public static Map<String, String> selectAdmin() throws Exception {
 
 		ConnexionBDD connect = ConnexionBDD.getInstance();
 
-		try{
-		    statement = connect.getCnx().createStatement();
-			preparedStatement = connect.getCnx().prepareStatement("SELECT * from ADMINS");
+		try {
+			statement = connect.getCnx().createStatement();
+			preparedStatement = connect.getCnx().prepareStatement("SELECT * from USER WHERE EST_ADMIN IS NOT NULL");
 			resultSet = preparedStatement.executeQuery();
 			listAdmin = new HashMap<>();
-		    while (resultSet.next()) {
-		    	String name = resultSet.getString("username");
-		       String email = resultSet.getString("email");
-		       String pass = resultSet.getString("password");
-		       listAdmin.put(email , pass);
-		    }
-		}
-		catch (Exception e) {
-		      throw e;
-		} 
-		finally {
-		      connect.closeCnx();
+			while (resultSet.next()) {
+				String name = resultSet.getString("NOM");
+				String email = resultSet.getString("E_MAIL");
+				String pass = resultSet.getString("PASSWORD");
+				listAdmin.put(email, pass);
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			connect.closeCnx();
 		}
 		return listAdmin;
 	}
